eaadffb9d1f6abc2061b13bf72f04c821d52cfe4
ayacai115 <aya.oki@japantaxi.co.jp>
2019-11-18 18:59:15 +0900

    initial commit: bundle gem my_pull_requests -b

---
+++ lib/my_pull_requests.rb
@@ -0,0 +1,6 @@
+require "my_pull_requests/version"
+
+module MyPullRequests
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/my_pull_requests_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe MyPullRequests do
+  it "has a version number" do
+    expect(MyPullRequests::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
